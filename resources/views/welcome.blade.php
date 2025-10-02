03f71000e545fabf9398234d8dd9e377badd352f
Gabriel Rodriguez <junior.gibson.lp@gmail.com>
2025-10-02 14:06:46 -0600

    Installing VueJS

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>GamerStore Inventory</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
-<body>
+<body id="app">
     <nav class="navbar navbar-expand bg-dark border-bottom border-body" data-bs-theme="dark">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">🎮 GamerStore</a>
@@ -29,6 +29,7 @@
 
 
     <div class="pt-3" style="padding:5%;">
+        <h1>@{{ message }}</h1>
         <div class="row">
         
             <div class="col-md-8">
@@ -114,5 +115,17 @@
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+
+    <!-- VueJS rules -->
+    <script>
+    const { createApp } = Vue;
+        createApp({
+            data() {
+                return {
+                    message: 'VueJS works!'
+                }
+            }
+        }).mount('#app');
+    </script>
 </body>
 </html>
